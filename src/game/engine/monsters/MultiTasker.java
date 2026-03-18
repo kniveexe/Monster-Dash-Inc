da7223d33281848ca1a24881747803d443c5c2bf
Mohamed Amr <mc.knive.lol@gmail.com>
2026-03-18 15:43:28 +0200

    New Subclasses

--- src/game/engine/monsters/MultiTasker.java
+++ src/game/engine/monsters/MultiTasker.java
@@ -3,3 +3,13 @@ package game.engine.monsters;
 import game.engine.Role;
 import game.engine.Constants;
 
+public class MultiTasker extends Monster{
+	int normalSpeedTurns;
+	public MultiTasker(String name, String description, Role originalRole, int energy) {
+	
+		super(name,description,originalRole,energy);
+		this.normalSpeedTurns = 0;
+	}
+	
+	
+}
--- src/game/engine/monsters/Schemer.java
+++ src/game/engine/monsters/Schemer.java
@@ -4,3 +4,14 @@ import game.engine.Role;
 import game.engine.Constants;
 import java.util.List;
 
+public class Schemer extends Monster{
+
+	public Schemer(String name, String description, Role originalRole, int energy) {
+		super(name, description, originalRole, energy);
+	}
+	
+	
+	
+	
+	
+}
