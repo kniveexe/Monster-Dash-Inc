f2c802de1606f1c6a70e2dbd69101404a9ca0b5d
Mohamed Amr <mc.knive.lol@gmail.com>
2026-03-18 15:31:18 +0200

    ssss

--- src/game/engine/monsters/Dasher.java
+++ src/game/engine/monsters/Dasher.java
@@ -2,4 +2,16 @@ package game.engine.monsters;
 
 import game.engine.Role;
 
-
+public class Dasher{
+	
+	int momentumTurns;
+	
+	public Dasher(String name, String description, Role role, int energy) {
+		
+		
+	}
+	
+	
+	
+	
+}
--- src/game/engine/monsters/Monster.java
+++ src/game/engine/monsters/Monster.java
@@ -2,4 +2,40 @@ package game.engine.monsters;
 
 import game.engine.Role;
 
-public abstract class Monster implements Comparable<Monster> {
+public abstract class Monster{
+	String name;
+	String description;
+	Role role;
+	Role originalrole;
+	int energy;
+	int position;
+	boolean frozen;
+	boolean shielded;
+	int confusionTurns;
+	
+	public Monster(String name, String description, Role originalRole, int energy) {
+		this.name = name;
+		this.description = description;
+		this.originalrole = originalRole;
+		this.energy = energy;
+		this.position = 0;
+		this.frozen = false;
+		this.shielded = false;
+		this.confusionTurns = 0;
+	}
+	
+	public int compareTo(Monster o) {
+	    return this.position - o.position;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
