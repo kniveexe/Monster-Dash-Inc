2c521c8ceec59855ae6bc5be68a0040e32e13faa
bodyw18600-design <bodyw18600@gmail.com>
2026-03-18 23:55:38 +0200

    hh

--- src/game/engine/cells/Cell.java
+++ src/game/engine/cells/Cell.java
@@ -1,3 +1,29 @@
 package game.engine.cells;
 
 import game.engine.monsters.Monster;
+public class Cell{
+	private String name; private Monster monster;  // READ AND WRITE
+
+    public Cell(String name) {
+        this.name    = name;
+        this.monster = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Monster getMonster() {
+        return monster;
+    }
+
+    public void setMonster(Monster monster) {
+        this.monster = monster;
+    }
+
+    
+    public String toString() {
+        return "Cell[" + name + "]";
+    }
+}
+	
