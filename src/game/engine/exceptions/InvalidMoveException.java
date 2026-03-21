7408d54c8c17bdff34043737c03651d19b50dd2b
Mohamed Amr <mc.knive.lol@gmail.com>
2026-03-21 17:57:20 +0200

    Almost Done

--- src/game/engine/exceptions/InvalidMoveException.java
+++ src/game/engine/exceptions/InvalidMoveException.java
@@ -1,5 +1,17 @@
 package game.engine.exceptions;
 
-public class InvalidMoveException {
-
+public class InvalidMoveException extends GameActionException {
+	private static final long serialVersionUID = 1L;
+	public static final String MSG  ="Invalid move attempted";
+	
+	public InvalidMoveException() {
+		super(MSG);
+		
+	}
+	public InvalidMoveException(String message) {
+		super(message);
+	}
+	
+	
+	
 }
